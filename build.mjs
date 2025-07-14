ac9e30de603e83a6675c96f82311992598637855
fsoich-redstage <74548121+fsoich-redstage@users.noreply.github.com>
2025-07-14 16:07:47 -0300

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -12,6 +12,75 @@ overrideGQLOperations([
     skipFragments: ['DOWNLOADABLE_ORDER_ITEMS_FRAGMENT'],
     operations: [],
   },
+  {
+
+    npm: '@dropins/storefront-checkout',
+
+    operations: [
+
+      `
+
+  fragment CHECKOUT_DATA_FRAGMENT on Cart {
+
+    available_payment_methods {
+
+      code
+
+      title
+
+      oope_payment_method_config {
+
+        backend_integration_url
+
+        custom_config {
+
+          ... on CustomConfigKeyValue {
+
+              key
+
+              value
+
+          }
+
+        }
+
+      }
+
+    }
+
+    selected_payment_method {
+
+      code
+
+      title
+
+      oope_payment_method_config {
+
+        backend_integration_url
+
+        custom_config {
+
+          ... on CustomConfigKeyValue {
+
+              key
+
+              value
+
+          }
+
+        }
+
+      }
+
+    }
+
+  }
+
+`,
+
+    ],
+
+  },
   // {
   //   npm: '@dropins/storefront-checkout',
   //   operations: [],
